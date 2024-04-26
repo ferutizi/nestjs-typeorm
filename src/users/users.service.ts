de93f992394e2f0af01066874a3bd3d9a921604b
ferutizi <fer.utizi@gmail.com>
2024-04-26 19:47:26 -0300

    user entity, module, controller, services

---
+++ src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  createSecureServer(user: CreateUserDto) {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+}
